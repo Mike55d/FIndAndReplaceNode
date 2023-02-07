7ad81a962dc8dc7273649c70d876d23c045003e5
MikeAdv <mike.gonzalez55d@gmail.com>
2023-02-07 01:15:00 -0400

    Fix constantes

--- findAndReplace.js
+++ findAndReplace.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const folder = './testFolder/';
+const stringToReplace = '/imagenes/BARRA/';
 const blue = "some shit text";
 
 
 const readFiles = () => {
     const files = fs.readdirSync(folder);
-    console.log('\x1b[34m',`Archivos encontrados : ${files.length}`,"\x1b[0m");
+    console.log('\x1b[34m', `Archivos encontrados : ${files.length}`, "\x1b[0m");
     let filesProcesed = 0;
     files.forEach(file => {
         const filePath = folder + file;
@@ -16,9 +17,8 @@ const readFiles = () => {
             }
             const sentences = data.split('\n');
             const newSentences = sentences.map((sentence) => {
-                const phpWay = '/imagenes/BARRA/".$phpEchoWay."/';
-                const htmlWay = '/imagenes/BARRA/<?php echo $htmlWay ?>/';
-                const stringToReplace = '/imagenes/BARRA/';
+                const phpWay = stringToReplace + '".$phpEchoWay."/';
+                const htmlWay = stringToReplace + '<?php echo $htmlWay ?>/';
                 const isPhpEcho = sentence.search('echo');
                 const isImage = sentence.search(stringToReplace);
                 if (isPhpEcho !== -1 && isImage !== -1) {
@@ -36,7 +36,7 @@ const readFiles = () => {
                     return;
                 }
             })
-            console.log("\x1b[32m",`archivo : ${file} LISTO!`,'\x1b[0m');
+            console.log("\x1b[32m", `archivo : ${file} LISTO!`, '\x1b[0m');
             filesProcesed++
             console.log(` Archivos procesados ${filesProcesed}`);
             console.log(` Faltan ${files.length - filesProcesed}`);
