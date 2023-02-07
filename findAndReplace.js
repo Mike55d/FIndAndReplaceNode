1d7ae542b5f0f9f17caed589abd6d2f856b5b065
MikeAdv <mike.gonzalez55d@gmail.com>
2023-02-07 00:55:03 -0400

    Finish script

--- findAndReplace.js
+++ findAndReplace.js
@@ -5,7 +5,8 @@ const blue = "some shit text";
 
 const readFiles = () => {
     const files = fs.readdirSync(folder);
-    console.log(files);
+    console.log('\x1b[34m',`Archivos encontrados : ${files.length}`,"\x1b[0m");
+    let filesProcesed = 0;
     files.forEach(file => {
         const filePath = folder + file;
         fs.readFile(filePath, 'utf8', (err, data) => {
@@ -16,7 +17,7 @@ const readFiles = () => {
             const sentences = data.split('\n');
             const newSentences = sentences.map((sentence) => {
                 const phpWay = '/imagenes/BARRA/".$phpEchoWay."/';
-                const htmlWay = '/imagenes/BARRA/<?php echo $testImage?>/';
+                const htmlWay = '/imagenes/BARRA/<?php echo $htmlWay ?>/';
                 const stringToReplace = '/imagenes/BARRA/';
                 const isPhpEcho = sentence.search('echo');
                 const isImage = sentence.search(stringToReplace);
@@ -35,38 +36,13 @@ const readFiles = () => {
                     return;
                 }
             })
-            console.log(`file : ${file} done`);
+            console.log("\x1b[32m",`archivo : ${file} LISTO!`,'\x1b[0m');
+            filesProcesed++
+            console.log(` Archivos procesados ${filesProcesed}`);
+            console.log(` Faltan ${files.length - filesProcesed}`);
         });
     })
 
 }
 
-// const readFile = () => {
-//     fs.readFile('./testFolder/testFile2.php', 'utf8', (err, data) => {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         const sentences = data.split('\n');
-//         const blue = sentences[5];
-//         console.log(blue.search('echo'));
-//         console.log(blue);
-//         const newSentences = sentences.map((sentence) => {
-//             const phpWay = '/imagenes/BARRA/".$phpEchoWay."/';
-//             const htmlWay = 'bluesss';
-//             const stringToReplace = '/imagenes/BARRA/';
-//             const isPhpEcho = sentence.search('echo');
-//             const isImage = sentence.search(stringToReplace);
-//             if (isPhpEcho && isImage) {
-//                 console.log(sentence);
-//                 return sentence.replace(stringToReplace, phpWay);
-//             }
-//             return sentence;
-//         });
-//         console.log(newSentences);
-//         console.log(`file : ${file} done`);
-//     });
-// }
-
 readFiles();
-// readFile();
